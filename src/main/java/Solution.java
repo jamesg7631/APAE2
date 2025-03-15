1550af3f369f2b0c07931a49d1f83e64f8e20ad4
James Glover <3057283g@student.gla.ac.uk>
2025-03-15 07:35:45 +0000

    Added rigorous validation to commands entered

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -10,6 +10,11 @@ public class Solution implements CommandRunner{
 
     public String runCommand(String command) {
         String[] parts = command.split(" ");
+
+        if (!isCommandValid(parts)) {
+            return "Invalid command";
+        }
+
         String commandType = parts[0];
 
         if (commandType.equals("start")) {
@@ -33,6 +38,50 @@ public class Solution implements CommandRunner{
             return taskManager.abort();
         } else {
             return "Invalid command";
+        }
+    }
+
+    public boolean isCommandValid(String[] parts) {
+        if ("start".equals(parts[0])) {
+            if (parts.length != 2) {
+                return false;
+            }
+            return isLong(parts[1]);
+        } else if ("cancel".equals(parts[0])) {
+            if (parts.length != 2) {
+                return false;
+            }
+            return isLong(parts[1]);
+        } else if ("running".equals(parts[0])) {
+            return parts.length == 1;
+        }
+        else if ("get".equals(parts[0])) {
+            if (parts.length != 2) {
+                return false;
+            }
+            return isLong(parts[1]);
+        } else if ("after".equals(parts[0])) {
+            if (parts.length != 3) {
+                return false;
+            }
+
+            if (!isLong(parts[1]) || !isLong(parts[2])) {
+                return false;
+            }
+        } else if ("finish".equals(parts[0])) {
+            return parts.length == 1;
+        } else if ("abort".equals(parts[0])) {
+            return parts.length == 1;
+        }
+        return false;
+    }
+
+    private boolean isLong(String number) {
+        try {
+            long longNumber = Long.parseLong(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
